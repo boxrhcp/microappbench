ae38ec84d8f61f18faee01836162694837e83c86
Fayan Leonardo Pardo <fayanpardo@gmail.com>
2020-04-25 14:13:57 +0200

    update

--- scripts/boot-sockshop.sh
+++ scripts/boot-sockshop.sh
@@ -6,14 +6,15 @@ istioctl manifest apply --set profile=demo --set values.tracing.enabled=true --s
 
 kubectl label namespace sock-shop istio-injection=enabled
 
-kubectl apply -f ../sockshop-istio/1-sock-shop-install/1-sock-shop-complete-demo-istio.yaml -n sock-shop
+kubectl apply -f ../sockshop-istio/1.1-sock-shop-install/1-sock-shop-complete-demo-istio.yaml -n sock-shop
 
-kubectl apply -f ../sockshop-istio/1-sock-shop-install/2-sockshop-gateway.yaml -n sock-shop
+kubectl apply -f ../sockshop-istio/1.1-sock-shop-install/2-sockshop-gateway.yaml -n sock-shop
 
-kubectl apply -f ../sockshop-istio/1-sock-shop-install/3-virtual-services-all.yaml -n sock-shop
+#kubectl apply -f ../sockshop-istio/1-sock-shop-install/3-virtual-services-all.yaml -n sock-shop
 
 sleep 60
 
 kubectl port-forward -n istio-system $(kubectl get pod -n istio-system -l app=zipkin -o jsonpath='{.items[0].metadata.name}') 9411:9411 &
 
 kubectl port-forward -n istio-system $(kubectl get pod -n istio-system -l app=kiali -o jsonpath='{.items[0].metadata.name}') 20001:20001 &
+
