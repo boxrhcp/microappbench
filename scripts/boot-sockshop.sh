5383aeee8a4ab358d577ff719c22bf633ab3673b
Fayan Leonardo Pardo <fayanpardo@gmail.com>
2020-05-12 12:16:36 +0200

    update pod affinity

--- scripts/boot-sockshop.sh
+++ scripts/boot-sockshop.sh
@@ -1,10 +1,25 @@
 gcloud container clusters create sock-shop --zone europe-west1-b --machine-type n1-standard-1 --num-nodes 4
 
+NODE_LIST=( $(kubectl get nodes --no-headers -o custom-columns=NAME:.metadata.name) )
+for index in ${!NODE_LIST[@]}; do
+    if [ $index == 0 ]
+    then
+        echo ${NODE_LIST[index]}
+        kubectl label nodes ${NODE_LIST[index]} benchmark=true
+    else
+        kubectl label nodes ${NODE_LIST[index]} benchmark=false
+    fi
+done
+
 kubectl create namespace sock-shop
 
 #istioctl manifest apply --set profile=demo --set values.tracing.enabled=true --set values.tracing.provider=zipkin --set values.pilot.traceSampling=100
 
 istioctl manifest apply --set profile=demo --set values.tracing.enabled=true --set values.pilot.traceSampling=100
+
+#kubectl create namespace istio-system
+
+#kubectl apply -f istio.yaml
 
 kubectl label namespace sock-shop istio-injection=enabled
 
