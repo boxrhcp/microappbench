3172ced1192d3b165ad26fbdf2c795cb59cbfb31
Fayan Leonardo Pardo <fayanpardo@gmail.com>
2020-06-13 11:41:17 +0200

    added flags script

--- scripts/run_openISBT/run-openISBT.sh
+++ scripts/run_openISBT/run-openISBT.sh
@@ -1,18 +1,44 @@
-cd ../../openISBT/openISBTBackend
-gradle buildMatchingTool
-gradle buildWorkloadGenerator
-gradle buildRunTool
-#gradle buildBoxPlotTool
+ #!/bin/bash
+flag_build=0
+address="http://localhost:80"
 
-cp build/libs/matchingTool-1.0-SNAPSHOT-all.jar ../../scripts/run_openISBT/
-cp build/libs/wlgenerator-1.0-SNAPSHOT-all.jar ../../scripts/run_openISBT/
-cp build/libs/runner-1.0-SNAPSHOT-all.jar ../../scripts/run_openISBT/
+for arg in "$@"
+do
+    case $arg in
+        -b|--build)
+        flag_build=1
+        shift # Remove --initialize from processing
+        ;;
+        -a|--address)
+        address="$2"
+        shift # Remove argument name from processing
+        shift # Remove argument value from processing
+        ;;
+    esac
+done
 
-cd ../openISBTWorker
-gradle clean build
-cp build/libs/openISBTWorker-1.0-SNAPSHOT.jar ../../scripts/run_openISBT/
+if [ $flag_build -eq 1 ]; then
+    echo "building openISBT"
+    cd ../../openISBT/openISBTBackend
+    gradle buildMatchingTool
+    gradle buildWorkloadGenerator
+    gradle buildRunTool
+    #gradle buildBoxPlotTool
 
-cd ../fakerServer
+    cp build/libs/matchingTool-1.0-SNAPSHOT-all.jar ../../scripts/run_openISBT/
+    cp build/libs/wlgenerator-1.0-SNAPSHOT-all.jar ../../scripts/run_openISBT/
+    cp build/libs/runner-1.0-SNAPSHOT-all.jar ../../scripts/run_openISBT/
+
+    cd ../openISBTWorker
+    gradle clean build
+    cp build/libs/openISBTWorker-1.0-SNAPSHOT.jar ../../scripts/run_openISBT/
+
+    cd ../fakerServer
+else
+    echo "ignoring openISBT build"
+    cd ../../openISBT/fakerServer
+fi
+
 node server.js &
 fakeServer_pid=$!
 
@@ -26,6 +52,6 @@ java -jar wlgenerator-1.0-SNAPSHOT-all.jar -o -m mapping-order.json -w workload-
 
 kill -KILL $fakeServer_pid
 
-java -jar runner-1.0-SNAPSHOT-all.jar -o -r results-order.json -w workload-order.json -e http://35.205.233.151 -t 5 -u localhost:8000
+java -jar runner-1.0-SNAPSHOT-all.jar -o -r results-order.json -w workload-order.json -e $address -t 5 -u localhost:8000
 
 kill -KILL $worker_pid
